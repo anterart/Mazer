459972629a87b848c82c4d4202f45068f5e7f4a0
yerez29 <yerez.29@gmail.com>
2019-09-16 19:00:37 +0300

    a random door is selected once flag is picked

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -6,29 +6,54 @@ using UnityEngine.SceneManagement;
 public class EndLevel : MonoBehaviour
 {
 
-    GameObject door;
+    GameObject door1;
+    GameObject door2;
+    GameObject door3;
+    GameObject door4;
+    public static bool doorOpened = false;
+    public static int doorNumber;
+    System.Random random = new System.Random();
     public int levelToLoad;
 
     private void Start()
     {
-        door = GameObject.Find("Door");
+        door1 = GameObject.Find("Door1");
+        door2 = GameObject.Find("Door2");
+        door3 = GameObject.Find("Door3");
+        door4 = GameObject.Find("Door4");
+        door1.GetComponent<Renderer>().enabled = false;
+        door2.GetComponent<Renderer>().enabled = false;
+        door3.GetComponent<Renderer>().enabled = false;
+        door4.GetComponent<Renderer>().enabled = false;
     }
     void Update()
     {
-        if (PlayerMovement.picked)
+        if (PlayerMovement.picked && !doorOpened)
         {
-            door.GetComponent<Renderer>().enabled = true;
-        }
-        else
-        {
-            door.GetComponent<Renderer>().enabled = false;
+            doorNumber = random.Next(0, 5);
+            switch (doorNumber)
+            {
+                case 1:
+                    door1.GetComponent<Renderer>().enabled = true;
+                    break;
+                case 2:
+                    door2.GetComponent<Renderer>().enabled = true;
+                    break;
+                case 3:
+                    door3.GetComponent<Renderer>().enabled = true;
+                    break;
+                default:
+                    door4.GetComponent<Renderer>().enabled = true;
+                    break;
+            }
+            doorOpened = true;
         }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player" && other.GetComponent<PlayerMovement>().isFlagPicked)
+        if (other.gameObject.tag == "Player" && other.GetComponent<PlayerMovement>().isFlagPicked && gameObject.name == "Door" + doorNumber.ToString())
         {
             SceneManager.LoadScene(levelToLoad);
         }
