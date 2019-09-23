bb48c59b1793ffede0d4f861b2036277535b87ce
yerez29 <yerez.29@gmail.com>
2019-09-24 00:47:39 +0300

    finished paused menu

---
+++ Assets/pauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scenes/MainMenu");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+
+    }
+}
